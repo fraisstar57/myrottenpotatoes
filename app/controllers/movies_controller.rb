46dbb568d231daaf64d2a897a733d3014bcbd086
Daniel Smitherman <daniel.smitherman.development@gmail.com>
2013-07-24 17:42:57 -0700

    Sorting on the column header! woot

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,43 @@
 class MoviesController < ApplicationController
-
+	
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
-  def index
-    @movies = Movie.all
+  def sort_movies
+	@sortOrder = params[:order]
+	if(params == nil || params[:order] == nil) 
+		@movies = @movies.sort_by{|e| e[:title]}
+		@sortOrder = "asc"
+	elsif (@sortOrder == "asc") 
+		@movies = @movies.sort_by{|e| e[:title]}
+		@sortOrder = "desc"
+	elsif (@sortOrder == "desc")
+		@movies = @movies.sort_by{|e| e[:title]}.reverse
+		@sortOrder = "asc"
+	end
+	if(@sortOrder != nil)
+		sortOrder = @sortOrder
+		params[:order] = @sortOrder
+		session[:order] = @sortOrder
+	end
+	movies = @movies
+  end
+  
+  attr_accessor:sortOrder
+  
+  def index  
+	@movies = Movie.all
+	if(session[:order] != nil) 
+		@sortOrder = session[:order]
+	elsif(@sortOrder == nil && session[:order] == nil)
+		@sortOrder = "none"
+	end
+	if(params[:order] == "none" || params[:order] == "asc" || params[:order] == "desc") 
+		sort_movies
+	end
   end
 
   def new

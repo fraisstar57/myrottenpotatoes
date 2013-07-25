9927de5f3e1e59be8b1dbd6573742e55d479f71d
Daniel Smitherman <daniel.smitherman.development@gmail.com>
2013-07-24 20:10:29 -0700

    Sorting for release_date implemented

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,7 +6,7 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
-  def sort_movies
+  def sort_movies_by_title
 	@titleSortOrder = params[:titleOrder]
 	if(params == nil || params[:titleOrder] == nil || params[:titleOrder] == "none") 
 		@movies = @movies.sort_by{|e| e[:title]}
@@ -23,13 +23,48 @@ class MoviesController < ApplicationController
 		params[:titleOrder] = @titleSortOrder
 		session[:titleOrder] = @titleSortOrder
 	end
+	@titleSortOrder = params[:titleOrder]
+	
 	movies = @movies
+	@titleSetCss = true
+  end
+  
+   
+  def sort_movies_by_release_date
+	
+    mapU = lambda { |x| x[:release_date].to_s.split(' ').values_at(0,1,2) }
+	
+	@releaseDateSortOrder = params[:releaseDateOrder]
+	if(params == nil || params[:releaseDateOrder] == nil || params[:releaseDateOrder] == "none") 
+		@movies = @movies.sort_by{|e| mapU.call(e)}
+		@releaseDateSortOrder = "desc"
+	elsif (@releaseDateSortOrder == "asc") 
+		@movies = @movies.sort_by{|e| mapU.call(e)}
+		@releaseDateSortOrder = "desc"
+	elsif (@releaseDateSortOrder == "desc")
+		@movies = @movies.sort_by{|e| mapU.call(e)}.reverse
+		@releaseDateSortOrder = "asc"
+	end
+	if(@releaseDateSortOrder != nil)
+		releaseDateSortOrder = @releaseDateSortOrder
+		params[:releaseDateOrder] = @releaseDateSortOrder
+		session[:releaseDateOrder] = @releaseDateSortOrder
+	end	
+	movies = @movies
+	@releaseDateSetCss = true
   end
   
   attr_accessor:titleSortOrder
   attr_accessor:releaseDateSortOrder
   
+  attr_accessor:titleSetCss
+  attr_accessor:releaseDateSetCss
+  
   def index  
+	
+	@titleSetCss = false
+	@releaseDateSetCss = false
+	
 	@movies = Movie.all
 	if(params[:titleOrder] != nil) 
 		session[:titleOrder] = params[:titleOrder]
@@ -41,8 +76,22 @@ class MoviesController < ApplicationController
 		session[:titleOrder] = @titleSortOrder
 	end
 	if(params[:titleOrder] == "none" || params[:titleOrder] == "asc" || params[:titleOrder] == "desc") 
-		sort_movies
+		sort_movies_by_title
 	end
+	
+	if(params[:releaseDateOrder] != nil) 
+		session[:releaseDateOrder] = params[:releaseDateOrder]
+	end
+	if(session[:releaseDateOrder] != nil) 
+		@releaseDateSortOrder = session[:releaseDateOrder]
+	elsif(@releaseDateSortOrder == nil && session[:releaseDateOrder] == nil)
+		@releaseDateSortOrder = "asc"
+		session[:releaseDateOrder] = @releaseDateSortOrder
+	end
+	if(params[:releaseDateOrder] == "none" || params[:releaseDateOrder] == "asc" || params[:releaseDateOrder] == "desc") 
+		sort_movies_by_release_date
+	end
+	
   end
 
   def new
